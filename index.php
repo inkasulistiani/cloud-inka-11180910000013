9c8b03f66fa79c00e3574bf3b072de912b95cfdb
inkasulistiani <71960949+inkasulistiani@users.noreply.github.com>
2020-09-27 15:21:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Cloud By Inka .S - 013</title>
+    	<meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- 
+Power Template 
+http://www.templatemo.com/tm-508-power
+-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo_misc.css">
+        <link rel="stylesheet" href="css/templatemo_style.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+
+        <div class="site-main" id="sTop">
+            <div class="site-header">
+                <div class="main-header">
+                    <div class="container">
+                        <div id="menu-wrapper">
+                            <div class="row">
+                                <nav class="navbar navbar-inverse" role="navigation">
+                                    <div class="navbar-header">
+                                        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                        <a href="#top" class="navbar-brand">Inka .S</a>
+                                        <div id="main-nav" class="collapse navbar-collapse">
+                                            <ul class="menu-first nav navbar-nav" style="margin-right: 20px;">
+                                                <li class="active"><a href="#">Home</a></li>                               
+                                            </ul>                                    
+                                        </div> <!-- /.main-menu -->
+                                    </div>
+                                </nav>
+                            </div> <!-- /.row -->
+                        </div> <!-- /#menu-wrapper -->                        
+                    </div> <!-- /.container -->
+                </div> <!-- /.main-header -->
+            </div> <!-- /.site-header -->
+        </div> <!-- /.site-main -->
+
+
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="banner-content">
+                            <h6>Created With Love</h6>
+                            <h2>Welcome To My Blog</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="intro">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>Hello!</h4>
+                        <h6>My name is Inka Sulistiani. You can call me Inka :)</h6>
+						<h6>I'm a student at UIN Syarif Hidayatullah Jakarta</h6>
+						<h6>On this blog, I will introduce some facilities available in my campus.</h6>
+						<h6>Enjoy! :)</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <section id="about" class="page-section">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="service-item first-service">
+                            <div class="icon">
+                                <img src="img/audit3.png" alt="">
+                            </div>
+                            <h4>Auditorium</h4>
+                            <p>Harun Nasution Auditorium. This room consists of two floors which can accommodate about a thousand people. Harun Nasution Auditorium is never devoid of the bustle of activities, such as seminars, workshops, and graduation processions.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="service-item second-service">
+                            <div class="icon">
+                                <img src="img/lab2.png" alt="">
+                            </div>
+                            <h4>Laboratory</h4>
+                            <p><br>Integrated Laboratory Center (Pusat Laboratorium Terpadu / PLT). Inside there is a laboratory for students majoring in chemistry, physics, mathematics, biology, agribusiness, information systems and informatics engineering.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="service-item third-service">
+                            <div class="icon">
+                                <img src="img/perpus5.png" alt="">
+                            </div>
+                            <h4>Library</h4>
+                            <p>Library Center UIN Syarif Hidayatullah Jakarta. This library center consists of 7 floors. In this library there are a lot of book collections which are very useful for students. This library is also open to the general public, not only for students at UIN Jakarta.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="portfolio" class="portfolio-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-heading">
+                            <h4>Photos</h4>
+                            <p>Below I show some photos of the facilities available at UIN Syarif Hidayatullah Jakarta</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="owl-portfolio" class="owl-carousel owl-theme imageGallery1">
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/masjid1.jpg" title=""><img src="img/masjid2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/audit1.jpg" title=""><img src="img/audit2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/lab1.jpg" title=""><img src="img/lab2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/perpus1.jpg" title=""><img src="img/perpus2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                 <a href="img/cangkir1.jpg" title=""><img src="img/cangkir2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/lab3.jpg" title=""><img src="img/lab4.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/perpus4.jpg" title=""><img src="img/perpus5.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/perpus6.jpg" title=""><img src="img/perpus7.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="portfolio-item">
+                                <a href="img/gedung1.jpg" title=""><img src="img/gedung2.jpg" alt="Gallery image 1" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contact-us">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-heading">
+                            <h4>Thank You!</h4>
+							<p>Until here the blog this time. <br>
+							Bye bye :)</p>
+							
+                        </div>
+                    </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12">
+                            <p>Copyright &copy; 2020 UIN Syarif Hidayatullah <br> 
+                            Inka Sulistiani - 11180910000013</p>
+                    </div>
+                    <div class="col-md-4 col-sm-12">
+                        <ul class="social-icons">
+                            <li><a href="https://instagram.com/inkajhn04?igshid=1iv27qs5xrah0">Instagram</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-md-offset-2 col-sm-12">
+                        <div class="back-to-top">
+                            <a href="#top">
+                                <i class="fa fa-angle-up"></i>
+                                back to top
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Map -->
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="js/vendor/jquery.gmap3.min.js"></script>
+
+        <script type="text/javascript">
+        $(document).ready(function() {
+            
+            // mobile nav toggle
+            $('#nav-toggle').on('click', function (event) {
+                event.preventDefault();
+                $('#main-nav').toggleClass("open");
+            });
+        });
+        </script>
+        
+        <!-- templatemo 406 flex -->
+    </body>
+</html>
